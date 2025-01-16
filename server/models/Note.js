73244e166aecf94fc3372124c6e9992604b301ff
vvhob <h2michalski@gmail.com>
2025-01-16 23:33:12 +0100

    updated models

--- server/models/Note.js
+++ server/models/Note.js
@@ -1,10 +1,12 @@
 class Note {
-    constructor(id, title, body, date = new Date().toISOString()) {
-      this.id = id;      
-      this.title = title; 
-      this.body = body;   
-      this.date = date;   
-    }
+  constructor(id, title, body, author, date = new Date().toISOString()) {
+      this.id = id;            
+      this.title = title;      
+      this.body = body;        
+      this.author = author;    
+      this.date = date;        
+      this.sharedWith = [];    
   }
-  
-  module.exports = Note;
+}
+
+module.exports = Note;
---
+++ server/models/User.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(id, username, email) {
+      this.id = id;           // User ID (int)
+      this.username = username; // Username
+      this.email = email;     // Email address
+    }
+  }
+  
+  module.exports = User;
+  
