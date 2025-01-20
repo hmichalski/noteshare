e45753bb31e351e957fcbc353110d2268681bf13
vvhob <h2michalski@gmail.com>
2025-01-20 21:00:28 +0100

    switched from notes to noteshare

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
-import Notes from './components/Notes/Noteshare';
+import Noteshare from './components/Noteshare/Noteshare';
 import NotFound from './components/NotFound/NotFound';
 import Home from './components/Home/Home';
 
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="noteshare" element={<Notes />} />
+          <Route path="noteshare" element={<Noteshare />} />
           {/* Catch-all route for 404 */}
           <Route path="*" element={<NotFound />} />
         </Route>
