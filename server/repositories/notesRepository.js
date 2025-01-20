e45f1604661fa6ae644c1ab4cdaef95b7076d0e6
vvhob <h2michalski@gmail.com>
2025-01-20 19:18:01 +0100

    fixed notesRepository.js

--- server/repositories/notesRepository.js
+++ server/repositories/notesRepository.js
@@ -1,13 +1,12 @@
 const fs = require('fs');
 const path = require('path');
+
 const notesFilePath = path.join(__dirname, '../data/notes.json');
 
-// fetch all notes from the JSON file
 const getAllNotes = () => {
   return JSON.parse(fs.readFileSync(notesFilePath, 'utf-8'));
 };
 
-// save notes to json file
 const saveNotes = (notes) => {
   fs.writeFileSync(notesFilePath, JSON.stringify(notes, null, 2), 'utf-8');
 };
@@ -23,14 +22,29 @@ const addNote = (note) => {
   saveNotes(notes);
 };
 
+const updateNoteById = (id, updatedNote) => {
+  const notes = getAllNotes();
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index === -1)
+    return null;
+
+  notes[index] = { ...notes[index], ...updatedNote };
+  saveNotes(notes);
+  return notes[index];
+};
+
+// Delete a note by ID
 const deleteNoteById = (id) => {
   const notes = getAllNotes();
   const index = notes.findIndex((note) => note.id === id);
-  if (index === -1) return null;
+
+  if (index === -1) 
+    return null;
 
   const deletedNote = notes.splice(index, 1)[0];
   saveNotes(notes);
   return deletedNote;
 };
 
-module.exports = { getAllNotes, findNoteById, addNote, deleteNoteById };
+module.exports = { getAllNotes, findNoteById, addNote, updateNoteById, deleteNoteById };
